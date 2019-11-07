f8ac73c418229ee1ef5393bead376f5003214685
Jakgrit <jakgrit_frame@hotmail.com>
2019-11-07 15:27:52 +0700

    Success Fragment

--- app/src/main/java/com/example/money/add/AddFragment.kt
+++ app/src/main/java/com/example/money/add/AddFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.money.R
+import com.example.money.databinding.FragmentAddBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -18,7 +21,13 @@ class AddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        val binding = DataBindingUtil.inflate<FragmentAddBinding>(inflater, R.layout.fragment_add, container, false)
+        binding.apply {
+            cancelBtn.setOnClickListener { thisView ->
+                thisView.findNavController().navigate(R.id.action_addFragment_to_menuFragment)
+            }
+        }
+        return binding.root
     }
 
 
