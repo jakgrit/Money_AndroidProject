7a61436ab4a9a89d92f581941aeb2b6e2c19d56c
Jakgrit <jakgrit_frame@hotmail.com>
2019-11-07 13:41:01 +0700

    first commit

---
+++ app/src/main/java/com/example/money/about/AboutFragment.kt
@@ -0,0 +1,28 @@
+package com.example.money.about
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+
+import com.example.money.R
+import com.example.money.databinding.FragmentAboutBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentAboutBinding>(inflater,R.layout.fragment_about,container,false)
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/example/money/login/LoginFragment.kt
@@ -0,0 +1,28 @@
+package com.example.money.login
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+
+import com.example.money.R
+import com.example.money.databinding.FragmentLoginBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,R.layout.fragment_login,container,false)
+        return binding.root
+    }
+
+
+}
