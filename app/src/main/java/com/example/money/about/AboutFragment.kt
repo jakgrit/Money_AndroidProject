710e43124d0380e7f0b1faaea4104cc4967927db
Jakgrit <jakgrit_frame@hotmail.com>
2019-11-08 21:36:06 +0700

    Shared

--- app/src/main/java/com/example/money/about/AboutFragment.kt
+++ app/src/main/java/com/example/money/about/AboutFragment.kt
@@ -1,12 +1,13 @@
 package com.example.money.about
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 import com.example.money.R
 import com.example.money.databinding.FragmentAboutBinding
@@ -21,8 +22,43 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentAboutBinding>(inflater,R.layout.fragment_about,container,false)
+        setHasOptionsMenu(true)
+        val args = AboutFragmentArgs.fromBundle(arguments!!)
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.drop_menu, menu)
+
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            // hide the menu item if it doesn't resolve
+            menu?.findItem(R.id.share)?.setVisible(false)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return NavigationUI.onNavDestinationSelected(item!!,
+//            view!!.findNavController())
+//                || super.onOptionsItemSelected(item)
+
+        when (item!!.itemId) {
+            R.id.share -> shareSuccess()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun getShareIntent() : Intent {
+        val args = AboutFragmentArgs.fromBundle(arguments!!)
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT,"Shared")
+        return shareIntent
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
 
 }
